40e07e2b75c05aed02f2f88e69992180de7a1fab
makaya2afonso <makaya2afonso@gmail.com>
2023-07-09 01:23:10 -0300

    add simpletable dependency

--- cmd/main.go
+++ cmd/main.go
@@ -13,6 +13,8 @@ func main() {
 	// Define flags
 	addTaskFlag := flag.Bool("add", false, "Create a new task")
 	completeTaskFlag := flag.Int("complete", 0, "Complete a task")
+	deleteTaskFlag := flag.Int("delete", 0, "Delete a task")
+	listAllTasksFlag := flag.Bool("list", false, "List all the tasks")
 	flag.Parse()
 
 	// create a pointer to the tasks slice
@@ -22,17 +24,27 @@ func main() {
 	taskManagerStore := &usecases.StoreTasksInFileManager{Tasks: tasks}
 	taskManagerCreate := &usecases.CreateTaskManager{Tasks: tasks}
 	taskManagerComplete := &usecases.CompleteTaskManager{Tasks: tasks}
+	taskManagerDelete := &usecases.DeleteTaskManager{Tasks: tasks}
+	taskManagerList := &usecases.ListAllTasksManager{Tasks: tasks}
 
 	// load tasks from file
 	tasksFromFile, _ := usecases.LoadTasksFromFile("tasks.json")
 
 	switch {
+	case *listAllTasksFlag:
+		taskManagerList.ListAllTasks(tasksFromFile)
+
 	case *addTaskFlag:
-		taskManagerCreate.CreateTask("First task")
-		taskManagerCreate.CreateTask("Second task")
-		err := taskManagerStore.StoreTasksInFile(tasksFromFile, "tasks.json")
+		task, err := usecases.GetUserInput(os.Stdin, flag.Args()...)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		taskManagerCreate.CreateTask(tasksFromFile, task)
+
+		error := taskManagerStore.StoreTasksInFile(tasksFromFile, "tasks.json")
+		if error != nil {
+			fmt.Fprintln(os.Stderr, error.Error())
 			os.Exit(1)
 		}
 
@@ -42,7 +54,18 @@ func main() {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
+		err = taskManagerStore.StoreTasksInFile(tasksFromFile, "tasks.json")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 
+	case *deleteTaskFlag >= 0:
+		err := taskManagerDelete.DeleteTask(tasksFromFile, *deleteTaskFlag)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 		err = taskManagerStore.StoreTasksInFile(tasksFromFile, "tasks.json")
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
